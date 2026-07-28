1f5f942fd825a94850caace6fc8aca5041593df1
AlWaha-Store <wallter10990@gmail.com>
2026-07-28 10:24:07 +0300

    Update next.config.js for image optimization and output

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
 const nextConfig = {
   images: {
     domains: ['vxjjlfuxsnjvttpspdar.supabase.co'],
+    unoptimized: true,
   },
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
 }
 
 module.exports = nextConfig 
